0da85eb00eba633164a6cee860a073d2ee054cae
nguyentientoi382 <nguyentientoi382@gmail.com>
2015-10-06 13:21:30 +0700

    update

---
+++ upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8084/tranhdongdep/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost:8084/tranhdongdep/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8084/tranhdongdep/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8084/tranhdongdep/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/tranhdongdep/upload/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/tranhdongdep/upload/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/tranhdongdep/upload/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/tranhdongdep/upload/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/tranhdongdep/upload/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/tranhdongdep/upload/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/tranhdongdep/upload/system/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/tranhdongdep/upload/system/download/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/tranhdongdep/upload/system/upload/');
+define('DIR_LOGS', 'C:/xampp/htdocs/tranhdongdep/upload/system/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/tranhdongdep/upload/system/modification/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/tranhdongdep/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'tranhdongdep');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'vntek_');
---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8084/tranhdongdep/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8084/tranhdongdep/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/tranhdongdep/upload/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/tranhdongdep/upload/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/tranhdongdep/upload/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/tranhdongdep/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/tranhdongdep/upload/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/tranhdongdep/upload/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/tranhdongdep/upload/system/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/tranhdongdep/upload/system/download/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/tranhdongdep/upload/system/upload/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/tranhdongdep/upload/system/modification/');
+define('DIR_LOGS', 'C:/xampp/htdocs/tranhdongdep/upload/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'tranhdongdep');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'vntek_');
